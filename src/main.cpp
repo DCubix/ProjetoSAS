3e6a5e6290e85b7d4cb05e5f4064a1c121e2dc90
DCubix <diego95lopes@live.com>
2019-09-04 17:57:24 -0400

    Ajustes e tradução de pacotes UDP

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <mutex>
 #include <future>
+#include <fstream>
 
 #ifdef _WIN32
 #include <stdio.h>
@@ -14,10 +15,11 @@
 
 #include "app.hpp"
 #include "headers.hpp"
+#include "../osdialog/OsDialog.hpp"
 #include "../imgui/imgui_memory_editor.h"
 
 struct Packet {
-	std::string from, to, type;
+	std::string from, to, type, desc;
 	std::vector<char> data;
 };
 
@@ -31,6 +33,7 @@ static std::vector<std::string> g_Addresses;
 static std::vector<Packet> g_Pacotes;
 static std::vector<char> g_SelectedData;
 static bool g_Sniffing = false;
+static bool g_ScrollToBottom = false;
 static HOSTENT* g_Local = nullptr;
 static std::mutex g_Lock;
 
@@ -152,8 +155,11 @@ void initializeSocket(int in) {
 	g_Sniffer = socket(AF_INET, SOCK_RAW, IPPROTO_IP);
 	if (g_Sniffer == INVALID_SOCKET) {
 		std::cout << "Socket inválido." << std::endl;
+		std::cout << "Erro: " << WSAGetLastError() << std::endl;
 		return;
 	}
+
+	listInterfaces();
 
 	if (g_Local == nullptr) {
 		closesocket(g_Sniffer);
@@ -196,22 +202,52 @@ void initializeSocket(int in) {
 			Packet pak{};
 			pak.from = std::string(inet_ntoa(src.sin_addr));
 			pak.to = std::string(inet_ntoa(dest.sin_addr));
-			pak.type = "???";
 
 			switch (iphdr->ip_protocol) {
-				case 1: { //ICMP Protocol
-
-				} break;
-
-				case 6: { //TCP Protocol
+				case 6: { // Protocolo TCP
 					auto tcp = (TCP_HDR*)(buff + iphdrlen);
 					pak.type = "TCP";
 					auto dataPtr = buff + iphdrlen + tcp->data_offset * 4;
 					int dataSize = (count - tcp->data_offset * 4 - iphdrlen);
 					pak.data = std::vector<char>(dataPtr, dataPtr + dataSize);
-				} break;
+					pak.from += ":" + std::to_string(tcp->source_port);
+					pak.to += ":" + std::to_string(tcp->dest_port);
 
-				case 17: { //UDP Protocol
+					std::string dat(pak.data.begin(), pak.data.end());
+					if (tcp->source_port == 80 ||
+						tcp->source_port == 8080 ||
+						tcp->source_port == 8008 ||
+						tcp->source_port == 591 ||
+						dat.find("HTTP/1.1") != dat.npos)
+					{
+						pak.type = "TCP:http";
+					}
+				} break;
+				case 17: { // Protocolo UDP
+					auto udp = (UDP_HDR*)(buff + iphdrlen);
+					pak.type = "UDP";
+					auto dataPtr = buff + iphdrlen + sizeof(UDP_HDR);
+					int dataSize = (count - sizeof(UDP_HDR) - iphdrlen);
+					pak.data = std::vector<char>(dataPtr, dataPtr + dataSize);
+					pak.from += ":" + std::to_string(udp->source_port);
+					pak.to += ":" + std::to_string(udp->dest_port);
+
+					std::string dat(pak.data.begin(), pak.data.end());
+					if (udp->source_port == 8080 ||
+						udp->source_port == 8008 ||
+						udp->source_port == 591 ||
+						dat.find("HTTP/1.1") != dat.npos)
+					{
+						pak.type = "UDP:http";
+					}
+				} break;
+				case 1: { // Protocolo ICMP
+					auto icmp = (ICMP_HDR*)(buff + iphdrlen);
+					pak.type = "ICMP";
+					pak.desc = "T: " + std::to_string(icmp->type) + " C: " + std::to_string(icmp->code);
+					auto dataPtr = buff + iphdrlen + sizeof(ICMP_HDR);
+					int dataSize = (count - sizeof(ICMP_HDR) - iphdrlen);
+					pak.data = std::vector<char>(dataPtr, dataPtr + dataSize);
 				} break;
 
 				default: break;
@@ -222,6 +258,7 @@ void initializeSocket(int in) {
 					g_Pacotes.erase(g_Pacotes.begin());
 				}
 				g_Pacotes.push_back(pak);
+				g_ScrollToBottom = true;
 			}
 
 			//Sleep(500);
@@ -229,14 +266,12 @@ void initializeSocket(int in) {
 	}
 
 	closesocket(g_Sniffer);
-	WSACleanup();
 }
 
 static ImGui::ColumnHeader headers[] = {
-	{ "#", 40 },
-	{ "De", 120 },
-	{ "Para", 120 },
-	{ "Tipo", 60 },
+	{ "Origem", 170 },
+	{ "Destino", 170 },
+	{ "Tipo", 80 },
 	{ "Dados", 50 }
 };
 
@@ -271,16 +306,16 @@ void gui() {
 		ImGui::BeginColumnHeadersSync("PacotesContent", headers, IM_ARRAYSIZE(headers), true);
 		int i = 0;
 		for (auto&& p : g_Pacotes) {
-			ImGui::Text(std::to_string(i).c_str());
-			ImGui::NextColumn();
 			ImGui::Text(p.from.c_str());
 			ImGui::NextColumn();
 			ImGui::Text(p.to.c_str());
 			ImGui::NextColumn();
 			ImGui::Text(p.type.c_str());
 			ImGui::NextColumn();
-			if (p.data.empty() || p.type == "???") {
-				ImGui::Text("??? (%d byte%s)", p.data.size(), p.data.empty() || p.data.size() > 0 ? "s" : "");
+			// ImGui::Text(p.desc.c_str());
+			// ImGui::NextColumn();
+			if (p.data.empty()) {
+				ImGui::Text("<VAZIO>");
 			} else {
 				const char* id = (std::string("Ver##") + std::to_string(i)).c_str();
 				if (ImGui::Button(id)) {
@@ -291,13 +326,27 @@ void gui() {
 			ImGui::NextColumn();
 			i++;
 		}
-		ImGui::SetScrollHereY(1.0f);
+		if (g_ScrollToBottom) {
+			ImGui::SetScrollHereY(1.0f);
+			g_ScrollToBottom = false;
+		}
 		ImGui::EndColumnHeadersSync(headers, IM_ARRAYSIZE(headers));
 	}
 	ImGui::End();
 
 	if (data_view_open) {
 		ImGui::Begin("Dados", &data_view_open);
+		if (ImGui::Button("Salvar")) {
+			osd::Filters flt("Arquivo de Dados:dat");
+			auto res = osd::Dialog::file(osd::DialogAction::SaveFile, ".", flt);
+			if (res.has_value()) {
+				std::ofstream of(res.value());
+				if (of.good()) {
+					of.write(g_SelectedData.data(), g_SelectedData.size());
+					of.close();
+				}
+			}
+		}
 		g_MemEdit.DrawContents(g_SelectedData.data(), g_SelectedData.size());
 		ImGui::End();
 	}
